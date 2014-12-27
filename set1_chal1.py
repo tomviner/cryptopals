4f73294424a875bad0353da2b4aa5d1537b749a2
TomV <tom@viner.tv>
2014-12-27 20:23:39 +0000

    chal 1 done, with decoded msg

--- set1_chal1.py
+++ set1_chal1.py
@@ -12,8 +12,25 @@ Cryptopals Rule
 Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 """
 
-def test_hex_to_base64():
-    input_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    expected_str = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-    assert hex_to_base64(input_hex) == expected_str
+from base64 import b64encode, b16decode
 
+
+def decode_hex(input_hex):
+    return b16decode(input_hex.upper(), casefold=True)
+
+def hex_to_base64(input_hex):
+    string = decode_hex(input_hex)
+    return b64encode(string)
+
+
+def test_hex_to_base64_easy():
+    input_hex = '78797A'
+    assert decode_hex(input_hex) == 'xyz'
+    expected_b64 = 'eHl6'
+    assert hex_to_base64(input_hex) == expected_b64
+
+def test_hex_to_base64_example():
+    input_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    assert decode_hex(input_hex) == "I'm killing your brain like a poisonous mushroom"
+    expected_b64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    assert hex_to_base64(input_hex) == expected_b64
