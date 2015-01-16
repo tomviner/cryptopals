ee6141e02f476e86ad823b03624f2b0977aaee85
TomV <tom@viner.tv>
2015-01-16 18:13:07 +0000

    complete chal 7 and 8

---
+++ cryptopals/set1_chal7.py
@@ -0,0 +1,120 @@
+"""
+AES in ECB mode
+The Base64-encoded content in this file has been encrypted via AES-128 in ECB mode under the key
+
+"YELLOW SUBMARINE".
+(case-sensitive, without the quotes; exactly 16 characters; I like "YELLOW SUBMARINE" because
+it's exactly 16 bytes long, and now you do too).
+
+Decrypt it. You know the key, after all.
+
+Easiest way: use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+
+Do this with code.
+You can obviously decrypt this using the OpenSSL command-line tool, but we're having you get
+ECB working in code for a reason. You'll need it a lot later on, and not just for attacking ECB.
+"""
+
+import re
+from base64 import b64decode
+from textwrap import dedent
+
+from Crypto.Cipher import AES
+
+
+def decrypt(password, ciphertext):
+    """ Python Cryptography Toolkit (pycrypto) """
+    decrypter = AES.new(password, AES.MODE_ECB)
+    return decrypter.decrypt(ciphertext)
+
+
+def test_decrypt():
+    # check we are using 128 bits as instructed
+    assert 128 == 8 * AES.block_size  # bits = 8 * bytes
+    cipher_b64 = open('cryptopals/set1_chal7.txt', 'rb').read()
+    ciphertext = b64decode(cipher_b64)
+    result = decrypt(password="YELLOW SUBMARINE", ciphertext=ciphertext)
+    expected = dedent("""
+        I'm back and I'm ringin' the bell
+        A rockin' on the mike while the fly girls yell
+        In ecstasy in the back of me
+        Well that's my DJ Deshay cuttin' all them Z's
+        Hittin' hard and the girlies goin' crazy
+        Vanilla's on the mike, man I'm not lazy.
+
+        I'm lettin' my drug kick in
+        It controls my mouth and I begin
+        To just let it flow, let my concepts go
+        My posse's to the side yellin', Go Vanilla Go!
+
+        Smooth 'cause that's the way I will be
+        And if you don't give a damn, then
+        Why you starin' at me
+        So get off 'cause I control the stage
+        There's no dissin' allowed
+        I'm in my own phase
+        The girlies sa y they love me and that is ok
+        And I can dance better than any kid n' play
+
+        Stage 2 -- Yea the one ya' wanna listen to
+        It's off my head so let the beat play through
+        So I can funk it up and make it sound good
+        1-2-3 Yo -- Knock on some wood
+        For good luck, I like my rhymes atrocious
+        Supercalafragilisticexpialidocious
+        I'm an effect and that you can bet
+        I can take a fly girl and make her wet.
+
+        I'm like Samson -- Samson to Delilah
+        There's no denyin', You can try to hang
+        But you'll keep tryin' to get my style
+        Over and over, practice makes perfect
+        But not if you're a loafer.
+
+        You'll get nowhere, no place, no time, no girls
+        Soon -- Oh my God, homebody, you probably eat
+        Spaghetti with a spoon! Come on and say it!
+
+        VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino
+        Intoxicating so you stagger like a wino
+        So punks stop trying and girl stop cryin'
+        Vanilla Ice is sellin' and you people are buyin'
+        'Cause why the freaks are jockin' like Crazy Glue
+        Movin' and groovin' trying to sing along
+        All through the ghetto groovin' this here song
+        Now you're amazed by the VIP posse.
+
+        Steppin' so hard like a German Nazi
+        Startled by the bases hittin' ground
+        There's no trippin' on mine, I'm just gettin' down
+        Sparkamatic, I'm hangin' tight like a fanatic
+        You trapped me once and I thought that
+        You might have it
+        So step down and lend me your ear
+        '89 in my time! You, '90 is my year.
+
+        You're weakenin' fast, YO! and I can tell it
+        Your body's gettin' hot, so, so I can smell it
+        So don't be mad and don't be sad
+        'Cause the lyrics belong to ICE, You can call me Dad
+        You're pitchin' a fit, so step back and endure
+        Let the witch doctor, Ice, do the dance to cure
+        So come up close and don't be square
+        You wanna battle me -- Anytime, anywhere
+
+        You thought that I was weak, Boy, you're dead wrong
+        So come on, everybody and sing this song
+
+        Say -- Play that funky music Say, go white boy, go white boy go
+        play that funky music Go white boy, go white boy, go
+        Lay down and boogie and play that funky music till you die.
+
+        Play that funky music Come on, Come on, let me hear
+        Play that funky music white boy you say it, say it
+        Play that funky music A little louder now
+        Play that funky music, white boy Come on, Come on, Come on
+        Play that funky music
+        \x04\x04\x04\x04""")
+    # my editor kills spaces at end of line, so put them back
+    expected = re.sub(r'(\n+)', r' \1', expected).lstrip()
+    assert result == expected
---
+++ cryptopals/set1_chal8.py
@@ -0,0 +1,57 @@
+"""
+Detect AES in ECB mode
+In this file are a bunch of hex-encoded ciphertexts.
+
+One of them has been encrypted with ECB.
+
+Detect it.
+
+Remember that the problem with ECB is that it is stateless and deterministic;
+the same 16 byte plaintext block will always produce the same 16 byte ciphertext.
+"""
+
+from collections import Counter
+from itertools import izip_longest
+
+
+# From http://stackoverflow.com/a/312644/15890
+def grouper(n, iterable, padvalue=None):
+    "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
+    return izip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
+
+
+def get_uniq_block_counts(cipher_lines):
+    return {
+        line: len(set(map(''.join, grouper(16, line))))
+        for line in cipher_lines
+    }
+
+def detect_ecb(line_to_uniq_chunks):
+    ecb_line = min(line_to_uniq_chunks, key=line_to_uniq_chunks.get)
+    return ecb_line, line_to_uniq_chunks[ecb_line]
+
+
+def test_detect_ecb():
+    block_len = 16
+    cipher_lines = open('cryptopals/set1_chal8.txt', 'rb').read().splitlines()
+    line_to_uniq_chunk_count = get_uniq_block_counts(cipher_lines)
+    num_uniq_blocks = line_to_uniq_chunk_count.values()
+    # if each line can fit 20 blocks of 16 bytes, we expect one line
+    # to have fewer than 20 unique blocks, and the rest no uniques,
+    # hence the full 20 unique blocks out of 20
+    blocks_long = len(cipher_lines[0]) / block_len
+    # this tells us one line has just 14/20 unique blocks, in other words
+    # it has repeat blocks of 16 bytes
+    uniq_blocks_in_detected_line = 14
+    assert dict(Counter(num_uniq_blocks)) == {
+        uniq_blocks_in_detected_line: 1,
+        blocks_long: len(cipher_lines) - 1
+    }
+    ecb_line, uniq_chunks = detect_ecb(line_to_uniq_chunk_count)
+    assert uniq_chunks == uniq_blocks_in_detected_line
+    assert ecb_line == (
+        'd880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd05'
+        '2f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1'
+        'd46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566'
+        '489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c'
+        '4040deb0ab51b29933f2c123c58386b06fba186a')
