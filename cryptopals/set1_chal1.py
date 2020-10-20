42dcd20c58de5fbbfa714d89b3846b4f84d577ab
Tom Viner <tom@viner.tv>
2020-10-20 22:29:42 +0100

    fix 1.1 and 1.2

--- cryptopals/set1_chal1.py
+++ cryptopals/set1_chal1.py
@@ -6,17 +6,21 @@ The string:
 Should produce:
 
 SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-So go ahead and make that happen. You'll need to use this code for the rest of the exercises.
+So go ahead and make that happen. You'll need to use this code for the rest of the
+exercises.
 
 Cryptopals Rule
-Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+Always operate on raw bytes, never on encoded strings. Only use hex and base64 for
+pretty-printing.
 """
 
 from base64 import b16decode, b64encode
 
 
 def decode_hex(input_hex):
+    'Hex to bytes'
     return b16decode(input_hex.upper(), casefold=True)
+
 
 def hex_to_base64(input_hex):
     string = decode_hex(input_hex)
@@ -25,12 +29,16 @@ def hex_to_base64(input_hex):
 
 def test_hex_to_base64_easy():
     input_hex = '78797A'
-    assert decode_hex(input_hex) == 'xyz'
-    expected_b64 = 'eHl6'
+    assert decode_hex(input_hex) == b'xyz'
+    expected_b64 = b'eHl6'
     assert hex_to_base64(input_hex) == expected_b64
 
+
 def test_hex_to_base64_example():
-    input_hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    assert decode_hex(input_hex) == "I'm killing your brain like a poisonous mushroom"
-    expected_b64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    input_hex = (
+        '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f757'
+        '3206d757368726f6f6d'
+    )
+    assert decode_hex(input_hex) == b"I'm killing your brain like a poisonous mushroom"
+    expected_b64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
     assert hex_to_base64(input_hex) == expected_b64
--- cryptopals/set1_chal2.py
+++ cryptopals/set1_chal2.py
@@ -23,8 +23,8 @@ def test_hex_xor_example():
     # input1 doesn't appear to be an encoded readable string
     input1 = '1c0111001f010100061a024b53535009181c'
     input2 = '686974207468652062756c6c277320657965'
-    assert decode_hex(input2) == "hit the bull's eye"
+    assert decode_hex(input2) == b"hit the bull's eye"
     result = hex_xor(input1, input2)
     expected = '746865206b696420646f6e277420706c6179'
-    assert decode_hex(result) == "the kid don't play"
+    assert decode_hex(result) == b"the kid don't play"
     assert result == expected
